026174822af15655ba99a0b6eea5871ad42e0c95
Fernando H. Zaghi <zaghi.fernando@gmail.com>
2019-12-24 00:12:06 -0300

    Commit inicial.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mi Card
+
+App desenvolvido com flutter, que exibe um cartão com foto de perfil, nome, telefone e e-mail.
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/fernando/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/fernando/Mobile/flutter/mi_card_flutter"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/home/fernando/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
